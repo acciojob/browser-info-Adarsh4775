a591587c4b1029112298456101813d5d5cd6a57b
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-15 21:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const browserName = navigator.appName;
+		const version = navigator.appVersion;
+
+		let message = "You are using " + browserName + " version " + version;
+
+		let extractDiv = document.getElementById("browser-info");
+		extractDiv.textContent = message;
